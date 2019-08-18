581fa656f31c0e06eb451d66f92d5c1440393b09
Prakash <pjha573@gmail.com>
2019-08-18 17:46:25 +0530

    LeftFiles

--- FileInputOutput.cpp
+++ FileInputOutput.cpp
@@ -1,18 +1,19 @@
 #include<iostream>
 #include<fstream>
-#include<map>
 using namespace std;
+
+
 int main()
 {
-	int A[100];
+	int InputArray[100];
 	int i=0;
-	int range,greatest=0;
+	int sizeOfRange,largestElement=0;
 	
-	int B[100];
-	memset(B,0,sizeof(B));
-	ifstream infile;
-	infile.open("/Users/prakashjha/Desktop/cleartax.txt");
-	if(infile.fail())
+	int Output[100];
+	memset(Output,0,sizeof(Output));
+	ifstream input;
+	input.open("/Users/prakashjha/Desktop/cleartax.txt");
+	if(input.fail())
 	{
 		cout<<"File not found";
 	}
@@ -21,60 +22,60 @@ int main()
 
 		string a,p;
 		int c=0;
-		while(infile>>a)
+		while(input>>a)
 		{
 			
 			if(a[a.length()-1]==',')
 			{
 
-				A[i]=stoi(a.substr(0,a.length()-1));
-				if(greatest<A[i])
-					greatest=A[i];
+				InputArray[i]=stoi(a.substr(0,a.length()-1));
+				if(largestElement<InputArray[i])
+					largestElement=InputArray[i];
 				i++;
 				
 			}
 			else if(c==0)
 			{
 				c=1;
-				A[i]=stoi(a.substr(0,a.length()));
-				if(greatest<A[i])
-					greatest=A[i];
+				InputArray[i]=stoi(a.substr(0,a.length()));
+				if(largestElement<InputArray[i])
+					largestElement=InputArray[i];
 				i++;
 				
 			}
 			else if(c==1)
 			{
-				range=stoi(a.substr(0,a.length()));
+				sizeOfRange=stoi(a.substr(0,a.length()));
 			}
 			
 			
 		}
-		infile.close(); 
-		infile.clear(); 
+		input.close(); 
+		input.clear(); 
 		int end;
-		if(greatest%range==0)
-			end=greatest/range-1;
+		if(largestElement%sizeOfRange==0)
+			end=largestElement/sizeOfRange-1;
 		else
-			end=greatest/range;
+			end=largestElement/sizeOfRange;
 
 		for(int j=0;j<i;j++)
 		{
-			if(A[j]%range==0)
-				B[(A[j]/range)-1]+=1;
+			if(InputArray[j]%sizeOfRange==0)
+				Output[(InputArray[j]/sizeOfRange)-1]+=1;
 			else
-				B[(A[j]/range)]+=1;
+				Output[(InputArray[j]/sizeOfRange)]+=1;
 		}
-		ofstream ofile;
-		ofile.open("/Users/prakashjha/Desktop/cleartaxoutput.txt");
+		ofstream OutputFile;
+		OutputFile.open("/Users/prakashjha/Desktop/cleartaxoutput.txt");
 
 		string s;
 
 		for(int j=0;j<=end;j++)
 		{
-			ofile<<range*j+1<<" to "<<range*(j+1)<<" : "<<B[j]<<endl;
+			OutputFile<<sizeOfRange*j+1<<" to "<<sizeOfRange*(j+1)<<" : "<<Output[j]<<endl;
 		}
-		infile.close(); 
-		infile.clear(); 
+		OutputFile.close(); 
+		OutputFile.clear(); 
 	}
 	return 0;
 }
